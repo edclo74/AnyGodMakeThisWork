c13bd466a072e38999bd2c693fd88a10082eef38
edclo74 <edclo74@kingshigh.school.nz>
2024-06-30 22:40:18 +1200

    hehe

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var SPEED = 300.0
+@export var SPEED = 100.0
 @export var ACCELERATION = 300.0
 @export var FRICTION = 300.0
 @onready var Sprite = $AnimatedSprite2D
@@ -9,27 +9,31 @@ var direction = Vector2.ZERO
 func _physics_process(delta):
 
 		
-		
+		var run = Input.is_action_pressed("run")
 		#Movement
-	direction = Input.get_vector("left", "right", "up", "down").normalized()
-	if direction:
-		velocity = velocity.move_toward(direction * SPEED, ACCELERATION)
-		Sprite.play("Running") 
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
-		Sprite.play("Idle")
+		direction = Input.get_vector("left", "right", "up", "down").normalized()
+
+		if direction and run:
+			velocity = velocity.move_toward(direction * SPEED * 1.5, ACCELERATION)
+			Sprite.play("Running") 
+		elif direction:
+			velocity = velocity.move_toward(direction * SPEED, ACCELERATION)
+			Sprite.play("Walking")
+		else:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+			Sprite.play("Idle")
 		
-	if velocity.x > 0:
-		Sprite.flip_h = true
-	elif velocity.x < 0:
-		Sprite.flip_h = false
+		if velocity.x > 0:
+			Sprite.flip_h = true
+		elif velocity.x < 0:
+			Sprite.flip_h = false
 	
 	#Shoot Mechanics
-	var shoot = Input.is_action_pressed("shoot")
-	if shoot:
-		Sprite.play("Shoot")
-	else:
-		pass
+		var shoot = Input.is_action_pressed("shoot")
+		if shoot:
+			Sprite.play("Shoot")
+		else:
+			pass
 
 func _process(delta):
 	look_at(get_global_mouse_position())
